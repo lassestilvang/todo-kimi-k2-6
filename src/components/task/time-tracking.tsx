765c54a31b0fe7d505aad5b08084544f583e9b79
Lasse Stilvang <lasse.stilvang@gmail.com>
2026-07-15 23:23:00 +0200

    refactor(components): update task component time-tracking.tsx

--- src/components/task/time-tracking.tsx
+++ src/components/task/time-tracking.tsx
@@ -6,7 +6,6 @@ import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import { Popover, PopoverContent, PopoverTrigger } from "@/components/ui/popover";
-import { cn } from "@/lib/utils";
 import type { TimeEntry } from "@/types";
 
 interface TimeTrackingProps {
