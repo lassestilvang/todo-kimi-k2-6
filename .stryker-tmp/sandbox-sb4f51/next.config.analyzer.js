1172f7c4960970c5fd75e190799841972c4f25ee
Lasse Stilvang <lasse.stilvang@gmail.com>
2026-07-28 23:23:00 +0200

    Remove deleted file next.config.analyzer.js

--- .stryker-tmp/sandbox-sb4f51/next.config.analyzer.js
+++
@@ -1,9 +0,0 @@
-// @ts-nocheck
-const BundleAnalyzer = require('@next/bundle-analyzer')({
-  analysisMode: 'static',
-  reportFilename: 'bundle-analysis.html',
-  openAnalyzer: false,
-});
-
-/** @type {import('next').NextConfig} */
-module.exports = BundleAnalyzer({});
