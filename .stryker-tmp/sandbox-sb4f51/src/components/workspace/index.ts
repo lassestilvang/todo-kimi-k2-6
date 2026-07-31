bd09c851f488469eda0e99be2ce055335fd25fe7
Lasse Stilvang <lasse.stilvang@gmail.com>
2026-07-31 23:23:00 +0200

    Remove deleted file index.ts

--- .stryker-tmp/sandbox-sb4f51/src/components/workspace/index.ts
+++
@@ -1,4 +0,0 @@
-// @ts-nocheck
-export { WorkspaceSelector } from "./workspace-selector";
-export { CreateWorkspaceDialog } from "./create-workspace-dialog";
-export { WorkspaceInvite, WorkspacePermissions } from "./workspace-selector";
