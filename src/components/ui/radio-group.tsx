ee6373ba5fc5ac4b3bfbcd3c24ac163150b6a4f7
Lasse Stilvang <lasse.stilvang@gmail.com>
2026-08-05 23:23:00 +0200

    feat(ui): add radio group component primitive

---
+++ src/components/ui/radio-group.tsx
@@ -0,0 +1,26 @@
+"use client"
+
+import * as React from "react"
+import * as RadioGroupPrimitive from "@radix-ui/react-radio-group"
+import { cn } from "@/lib/utils"
+
+const RadioGroup = RadioGroupPrimitive.Root
+
+const RadioGroupItem = React.forwardRef<
+  React.ElementRef<typeof RadioGroupPrimitive.Item>,
+  React.ComponentPropsWithoutRef<typeof RadioGroupPrimitive.Item>
+>(({ className, ...props }, ref) => (
+  <RadioGroupPrimitive.Item
+    ref={ref}
+    className={cn(
+      "flex h-4 w-4 items-center justify-center rounded-full border border-primary",
+      "focus:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2",
+      "disabled:cursor-not-allowed disabled:opacity-50",
+      className
+    )}
+    {...props}
+  />
+))
+RadioGroupItem.displayName = RadioGroupPrimitive.Item.displayName
+
+export { RadioGroup, RadioGroupItem }
