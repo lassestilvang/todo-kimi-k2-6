7c1a84ab172166b31a8be7de6d813aae4b12ac9d
Lasse Stilvang <lasse.stilvang@gmail.com>
2026-08-14 23:23:00 +0200

    chore(storybook): format storybook main configuration

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,19 +1,19 @@
-import type { StorybookConfig } from "@storybook/react-vite";
+import type { StorybookConfig } from '@storybook/react-vite';
 
 const config: StorybookConfig = {
-  stories: ["../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
+  stories: ['../src/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
   addons: [
-    "@storybook/addon-essentials",
-    "@storybook/addon-interactions",
-    "@storybook/addon-a11y",
+    '@storybook/addon-essentials',
+    '@storybook/addon-interactions',
+    '@storybook/addon-a11y',
   ],
   framework: {
-    name: "@storybook/react-vite",
+    name: '@storybook/react-vite',
     options: {},
   },
-  staticDirs: [{ from: "../public", to: "/public" }],
+  staticDirs: [{ from: '../public', to: '/public' }],
   docs: {
-    autodocs: "tag",
+    autodocs: 'tag',
   },
 };
 
