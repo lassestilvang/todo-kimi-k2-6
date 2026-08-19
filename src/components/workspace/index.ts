625239cb4abdeb902482e97a2a9dc8223bf0e714
Lasse Stilvang <lasse.stilvang@gmail.com>
2026-08-19 23:23:00 +0200

    feat(components): format workspace components module exports

--- src/components/workspace/index.ts
+++ src/components/workspace/index.ts
@@ -1,3 +1,3 @@
-export { WorkspaceSelector } from "./workspace-selector";
-export { CreateWorkspaceDialog } from "./create-workspace-dialog";
-export { WorkspaceInvite, WorkspacePermissions } from "./workspace-selector";
+export { WorkspaceSelector } from './workspace-selector';
+export { CreateWorkspaceDialog } from './create-workspace-dialog';
+export { WorkspaceInvite, WorkspacePermissions } from './workspace-selector';
