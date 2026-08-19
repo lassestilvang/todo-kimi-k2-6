0017cf4414af12e270823bfc02295cebb02b2f6d
Lasse Stilvang <lasse.stilvang@gmail.com>
2026-08-19 23:23:00 +0200

    feat(ui): format radio group component

--- src/components/ui/radio-group.tsx
+++ src/components/ui/radio-group.tsx
@@ -1,10 +1,10 @@
-"use client"
+'use client';
 
-import * as React from "react"
-import * as RadioGroupPrimitive from "@radix-ui/react-radio-group"
-import { cn } from "@/lib/utils"
+import * as React from 'react';
+import * as RadioGroupPrimitive from '@radix-ui/react-radio-group';
+import { cn } from '@/lib/utils';
 
-const RadioGroup = RadioGroupPrimitive.Root
+const RadioGroup = RadioGroupPrimitive.Root;
 
 const RadioGroupItem = React.forwardRef<
   React.ElementRef<typeof RadioGroupPrimitive.Item>,
@@ -13,14 +13,14 @@ const RadioGroupItem = React.forwardRef<
   <RadioGroupPrimitive.Item
     ref={ref}
     className={cn(
-      "flex h-4 w-4 items-center justify-center rounded-full border border-primary",
-      "focus:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2",
-      "disabled:cursor-not-allowed disabled:opacity-50",
+      'flex h-4 w-4 items-center justify-center rounded-full border border-primary',
+      'focus:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2',
+      'disabled:cursor-not-allowed disabled:opacity-50',
       className
     )}
     {...props}
   />
-))
-RadioGroupItem.displayName = RadioGroupPrimitive.Item.displayName
+));
+RadioGroupItem.displayName = RadioGroupPrimitive.Item.displayName;
 
-export { RadioGroup, RadioGroupItem }
+export { RadioGroup, RadioGroupItem };
