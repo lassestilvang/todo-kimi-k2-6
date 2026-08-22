8f81e8bfc08e0bda7846b54178f2cb670af84007
Lasse Stilvang <lasse.stilvang@gmail.com>
2026-08-22 23:23:00 +0200

    feat(types): format next-pwa ambient type declarations

--- src/types/next-pwa.d.ts
+++ src/types/next-pwa.d.ts
@@ -1,5 +1,5 @@
-declare module "next-pwa" {
-  import { NextConfig } from "next";
+declare module 'next-pwa' {
+  import { NextConfig } from 'next';
 
   interface PWACacheEntry {
     urlPattern: RegExp;
